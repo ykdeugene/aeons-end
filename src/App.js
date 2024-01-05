5af5c9990236a716a67bac728cfe101afe0f2a31
Eugene <ykdeugene@gmail.com>
2024-01-06 02:47:14 +0800

    recoded business logic

--- src/App.js
+++ src/App.js
@@ -1,48 +1,88 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 function App() {
-  const [turnOrderDeck, setTurnOrderDeck] = useState([
-    "Player 1 ",
-    "Player 2 ",
-    "Player 3 ",
-    "Player 4 ",
-    "Nemesis ",
-    "Nemesis ",
-  ])
-  const [turnOrderHistory, setTurnOrderHistory] = useState([])
-  const [cardDraw, setCardDraw] = useState(0)
+  const [turnOrderDeck, setTurnOrderDeck] = useState(
+    ["Player 1", "Player 2", "Player 3", "Player 4", "Nemesis", "Nemesis"].sort(
+      () => Math.random() - 0.5
+    )
+  )
 
-  function shuffleTO() {
-    var newTurnOrderDeck = [...turnOrderDeck]
-    newTurnOrderDeck.sort(() => Math.random() - 0.5)
-    console.log(newTurnOrderDeck)
-    setTurnOrderDeck(newTurnOrderDeck)
-  }
+  const [historyDeck, setHistoryDeck] = useState([])
+  const [cardNow, setCardNow] = useState("")
+  const [nextCard, setNextCard] = useState(turnOrderDeck[0])
 
-  function nextTurn() {
-    var nextCard = turnOrderHistory.concat(turnOrderDeck[cardDraw])
-    setTurnOrderHistory(nextCard)
-    card()
-  }
-
-  function card() {
-    if (cardDraw + 1 > 5) {
-      setCardDraw(0)
+  /**
+   * - If no more cards can be drawn from turnOrderDeck:
+   *    - cardNow is "No more Cards"
+   * - Else:
+   *    - Draws a card from the turnOrderDeck
+   *    - Updates cardNow to turnOrderDeck[0]
+   *    - Updates turnOrderDeck to remove cardNow
+   *    - Updates nextCard to turnOrderDeck[0]
+   *    - Updates HistoryDeck to include cardNow
+   */
+  function drawCard() {
+    let closeDeck = [...turnOrderDeck]
+    if (0 === closeDeck.length) {
+      setCardNow("No more Cards")
     } else {
-      setCardDraw(cardDraw + 1)
+      let cardDrawn = closeDeck[0]
+      setCardNow(cardDrawn)
+      closeDeck.splice(0, 1)
+      if (0 === closeDeck.length) {
+        setNextCard("No more Cards")
+      } else {
+        setNextCard(closeDeck[0])
+      }
+      setTurnOrderDeck(closeDeck)
+
+      let openDeck = [...historyDeck]
+      openDeck.push(cardDrawn)
+      setHistoryDeck(openDeck)
     }
   }
 
-  useEffect(() => {}, [turnOrderDeck])
+  /**
+   * - Shuffles only the existing cards remaining in the turnOrderDeck
+   * - Updates nextCard to turnOrderDeck[0]
+   */
+  function shuffleDeck() {
+    let closeDeck = turnOrderDeck.sort(() => Math.random() - 0.5)
+    setNextCard(closeDeck[0])
+    setTurnOrderDeck(closeDeck)
+  }
+
+  /**
+   * - Resets the turnOrderDeck with all 6 cards shuffled
+   * - Updates nextCard to turnOrderDeck[0]
+   */
+  function resetDeck() {
+    let closeDeck = [
+      "Player 1",
+      "Player 2",
+      "Player 3",
+      "Player 4",
+      "Nemesis",
+      "Nemesis",
+    ].sort(() => Math.random() - 0.5)
+    setTurnOrderDeck(closeDeck)
+    setNextCard(closeDeck[0])
+  }
+
+  function debug() {
+    console.log(turnOrderDeck)
+  }
 
   // HTML
   return (
     <div>
-      <div>Card Draw: {cardDraw}</div>
-      <div>Turn Order Deck: {turnOrderDeck}</div>
-      <div>Turn Order History: {turnOrderHistory}</div>
-      <button onClick={nextTurn}>Next Turn</button>
-      <button onClick={shuffleTO}>Shuffle TurnOrderDeck</button>
+      <div>historyDeck: {historyDeck}</div>
+      <div>cardNow: {cardNow}</div>
+      <div>nextCard: {nextCard}</div>
+      <button onClick={drawCard}>Draw a Card</button>
+      <button onClick={shuffleDeck}>Shuffle Deck</button>
+      <button onClick={resetDeck}>Reset Deck</button>
+      <button onClick={debug}>debug</button>
     </div>
   )
 }
